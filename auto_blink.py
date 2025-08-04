14fdb1ac2de7b268c79724f0c50903a8ba883440
Louis31423142 <louiswood3142@gmail.com>
2025-08-04 15:33:23 +0100

    added functionality to auto test script

--- auto_blink.py
+++ auto_blink.py
@@ -3,8 +3,14 @@ import os
 import argparse
 import serial
 import json
+from time import sleep
+
+# define an attempt limit for every programme
+# this might be better included in the json per programme
+tries = 10
 
 # Set up argument parsing
+# Unused currently, but might be useful
 parser = argparse.ArgumentParser(
                     prog='auto-test',
                     description='run given programme on selected target',
@@ -14,16 +20,16 @@ parser.add_argument('--file')
 parser.add_argument('--target')
 
 args = parser.parse_args()
-print(args.file, args.target)
+print('Arguments:', args.file, args.target)
 
-path_list = [{'dut_file': 'hello_world/usb/hello_usb.elf',
-                'buddy_file': 'hello_world/usb/hello_usb.elf',
-                'success_strings': ['string1', 'string2'],
-                'test_name': 'TEST1'},
+path_list = [{'dut_file': 'hello_world/serial/hello_serial.elf',
+                'buddy_file': 'NULL',
+                'success_string': 'Hello, world!',
+                'test_name': 'hello_serial on dut and buddy'},
             {'dut_file': 'blink/blink.elf',
-                'buddy_file': 'blink/blink.elf',
-                'success_strings': ['string1', 'string2'],
-                'test_name': 'TEST2'}           
+                'buddy_file': 'NULL',
+                'success_string': ['string1', 'string2'],
+                'test_name': 'Blink test'}           
             ]
 
 json_path_list = json.dumps(path_list)
@@ -39,44 +45,54 @@ with open("files.json", "r") as file:
 for element in data:
     print(element["dut_file"])
 
-# Set up pyserial for monitoring UART
-# Read every 1 second
-port0 = serial.Serial("/dev/ttyACM0", baudrate = 115200, timeout = 1.0)
-port1 = serial.Serial("/dev/ttyACM1", baudrate = 115200, timeout = 1.0)
-
 # For each dictionary in data, load the appropriate elfs onto dut and buddy
-for element in data:
-    # Announce test name
-    print('-----', element["test_name"], '-----')
+def main():
+    for element in data:
+        # Announce test name
+        print('-----', element["test_name"], '-----')
 
-    # Set target to dut
-    target_result = subprocess.run(["cargo", "run", "target", "dut"], cwd = "/home/louis/testing/testctl", capture_output = True, text = True)
+        # Set target to dut
+        set_target('dut')
+
+        # Flash elf onto DUT
+        result = subprocess.run(f'~/pico/openocd/src/openocd -s ~/pico/openocd/tcl -f interface/cmsis-dap.cfg -f target/rp2350.cfg -c "adapter speed 5000; program /home/louis/pico/pico_examples/build/{element["dut_file"]} verify reset exit"', capture_output = True, text = True, shell = True)
+        '''
+        print(result.stdout)
+        print(result.stderr)
+        '''
+
+        # If there is a buddy file specified (e.g an i2c emulator), switch target and flash the elf
+        if element["buddy_file"] != 'NULL':
+            set_target('buddy')
+            result = subprocess.run(f'~/pico/openocd/src/openocd -s ~/pico/openocd/tcl -f interface/cmsis-dap.cfg -f target/rp2350.cfg -c "adapter speed 5000; program /home/louis/pico/pico_examples/build/{element["buddy_file"]} verify reset exit"', capture_output = True, text = True, shell = True)
+            '''
+            print(result.stdout)
+            print(result.stderr)
+            '''
+
+        # Now wait for either timeout or success
+        check_for_string(tries, '/dev/ttyACM1')
+
+
+# Function reads UART and checks for specified success string
+def check_for_string(attempt_limit, port):
+    line = b''
+    attempts = 0
+
+    expected = bytes(f"{element['success_string']}\r\n", 'utf-8')
+
+    while line != expected and attempts < attempt_limit:
+        with serial.Serial(port, 115200, timeout=1) as ser:
+            line = ser.readline()
+            print('ACM1', line)
+            attempts += 1
+
+            sleep(1)
+
+# Function for setting target device (either dut or buddy)
+def set_target(target):
+    target_result = subprocess.run(["cargo", "run", "target", target], cwd = "/home/louis/testing/testctl", capture_output = True, text = True)
     print(target_result.stdout)
 
-    # Flash elf onto DUT
-    result = subprocess.run(f'~/pico/openocd/src/openocd -s ~/pico/openocd/tcl -f interface/cmsis-dap.cfg -f target/rp2350.cfg -c "adapter speed 5000; program /home/louis/pico/pico_examples/build/{element["dut_file"]} verify reset exit"', capture_output = True, text = True, shell = True)
 
-    print(result.stdout)
-    print(result.stderr)
-
-    # Set target to buddy
-    target_result = subprocess.run(["cargo", "run", "target", "buddy"], cwd = "/home/louis/testing/testctl", capture_output = True, text = True)
-    print(target_result.stdout)
-
-    # Flash elf onto buddy
-    result = subprocess.run(f'~/pico/openocd/src/openocd -s ~/pico/openocd/tcl -f interface/cmsis-dap.cfg -f target/rp2350.cfg -c "adapter speed 5000; program /home/louis/pico/pico_examples/build/{element["buddy_file"]} verify reset exit"', capture_output = True, text = True, shell = True)
-
-    print(result.stdout)
-    print(result.stderr)
-
-
-
-
-# Now monitor serial
-'''
-while True:
-    rcv0 = port0.read()
-    rcv1 = port1.read()
-    print("port 0 recieved: ", rcv0)
-    print("port 1 recieved: ", rcv1)
-'''
+main()
