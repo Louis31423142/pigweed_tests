1e00b6c582aab5b6158a3061bd063c3a30db332e
Louis31423142 <louiswood3142@gmail.com>
2025-08-07 16:45:03 +0100

    added results json

--- auto-tester.py
+++ auto-tester.py
@@ -6,14 +6,13 @@ import json
 from time import sleep
 import threading
 
-# define an attempt limit for every programme
-# this might be better included in the json per programme
-timeout = 3
-
 # Set up argument parsing
 parser = argparse.ArgumentParser(
-                    prog='auto-test',
-                    description='run tests from json',)
+                    prog = 'auto-test',
+                    description = 'Specify the DUT port, buddy port and optionally the location of \
+                        some files you want to test. The script will automatically open out all folders \
+                        and locate any .elf files to test, and puts them in a .json where you can specify \
+                        success strings and buddy files.')
 
 parser.add_argument('--dut')
 parser.add_argument('--buddy')
@@ -22,18 +21,25 @@ parser.add_argument('--root')
 args = parser.parse_args()
 print('Arguments:', args.dut, args.buddy)
 
+# After flashing the DUT, wait this long before checking for success strings
+timeout = 3
+
 captured_msgs = []
 
-# For each dictionary in data, load the appropriate elfs onto dut and buddy
+# For each dictionary in data, load the appropriate elfs onto dut and buddy and check for success strings
 def main():
     global captured_msgs
+
+    # Initialise results json
+    with open("results.json", "w") as file:
+        json.dump(["RESULTS:"], file, indent=4) 
 
     # If the user passed a root directory to expand into files, create the json based on this
     if args.root != None:
         load_json(args.root)
     
     # Read files for testing from files.json
-    with open("files.json", "r") as file:
+    with open("new_files.json", "r") as file:
         test_data = json.load(file)
 
     for element in test_data:
@@ -44,29 +50,46 @@ def main():
         # If there is a buddy file specified (e.g an i2c emulator), switch target and flash the elf
         if element["buddy_file"] != 'NULL':
             set_target('buddy')
-            result = subprocess.run(f'~/pico/openocd/src/openocd -s ~/pico/openocd/tcl -f interface/cmsis-dap.cfg -f target/rp2350.cfg -c "adapter speed 5000; program /home/louis/auto-tests/emulators/build/{element["buddy_file"]} verify reset exit"', capture_output = True, text = True, shell = True)
+            result = subprocess.run(f'~/pico/openocd/src/openocd \
+                                    -s ~/pico/openocd/tcl \
+                                    -f interface/cmsis-dap.cfg \
+                                    -f target/rp2350.cfg \
+                                    -c "adapter speed 5000; \
+                                    program /home/louis/auto-tests/emulators/build/{element["buddy_file"]} \
+                                    verify reset exit"', \
+                                    capture_output = True, text = True, shell = True)
 
             #print(result.stdout)
             #print(result.stderr)
-
 
         # Set target to dut
         set_target('dut')
 
         # Flash elf onto DUT
-        result = subprocess.run(f'~/pico/openocd/src/openocd -s ~/pico/openocd/tcl -f interface/cmsis-dap.cfg -f target/rp2350.cfg -c "adapter speed 5000; program {element["dut_file"]} verify reset exit"', capture_output = True, text = True, shell = True)
+        result = subprocess.run(f'~/pico/openocd/src/openocd \
+                                -s ~/pico/openocd/tcl \
+                                -f interface/cmsis-dap.cfg \
+                                -f target/rp2350.cfg \
+                                -c "adapter speed 5000; \
+                                program {element["dut_file"]} \
+                                verify reset exit"', \
+                                capture_output = True, text = True, shell = True)
 
         #print(result.stdout)
         #print(result.stderr)
 
         # Allow timeout seconds for test files to run before checking for success strings
         sleep(timeout)
-        #print(captured_msgs)
+        print(captured_msgs)
 
         if check_for_string(captured_msgs, element):
             print("TEST SUCCESFUL")
+            write_json(f"Test {element["dut_file"]} succeeded", "results.json")
         else:
             print("TEST FAILED")
+            write_json(f"Test {element["dut_file"]} failed.", "results.json")
+
+        # Add result to output json
 
         # Empty list for next test
         captured_msgs = []
@@ -92,7 +115,7 @@ def set_target(target):
 
 # Function for loading json with pico examples .elf files
 def load_json(root):
-    #expand root to full path
+    # Expand root to full path
     path = os.path.expanduser(f"{root}")
 
     file_path_list = []
@@ -124,6 +147,16 @@ def load_json(root):
     with open("new_files.json", "w") as file:
         file.write(json_data)
 
+# Function for adding results to json
+def write_json(new_data, filename):
+    with open(filename, 'r+') as file:
+        file_data = json.load(file)
+        file_data.append(new_data)
+
+        file.seek(0)
+
+        json.dump(file_data, file, indent=4)
+
 # Serial port reading 
 dut_port = serial.Serial(port = f"/dev/{args.dut}", baudrate = 115200)
 buddy_port = serial.Serial(port = f"/dev/{args.buddy}", baudrate = 115200)
@@ -133,11 +166,9 @@ def ReceiveThread():
     while True:
         if dut_port.inWaiting() > 0: # number of bytes in input buffer > 1
             dut_msg = dut_port.readline()
-            #print("DUT: ", dut_msg)
             captured_msgs.append(dut_msg)
         elif buddy_port.inWaiting() > 0:
             buddy_msg = buddy_port.readline()
-            #print("BUDDY: ", buddy_msg)
             captured_msgs.append(buddy_msg)
         else:    
             sleep(0.1)
