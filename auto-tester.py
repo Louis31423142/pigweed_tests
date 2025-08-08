75d243eaa663de0e04b16350bb2040abf9d9232d
Louis31423142 <louiswood3142@gmail.com>
2025-08-08 11:26:49 +0100

    improved file search and arg parsing

--- auto-tester.py
+++ auto-tester.py
@@ -8,25 +8,36 @@ import threading
 
 # Set up argument parsing
 parser = argparse.ArgumentParser(
-                    prog = 'auto-test',
-                    description = 'Specify the DUT port, buddy port and optionally the location of \
-                        some files you want to test. The script will automatically open out all folders \
+                    prog = 'auto-tester',
+                    description = 'Specify the DUT port, buddy port and either a json with tests inside, \
+                        or the path to a build folder containing some .elf files you would like to test \
+                        The script will automatically open out all folders \
                         and locate any .elf files to test, and puts them in a .json where you can specify \
                         success strings and buddy files.')
 
-parser.add_argument('--dut')
-parser.add_argument('--buddy')
-parser.add_argument('--root')
+parser.add_argument('--dut', required=True)
+parser.add_argument('--buddy', required=True)
+
+# Required group so user has to submit either a root or a tests json
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('--root')
+group.add_argument('--tests')
 
 args = parser.parse_args()
-print('Arguments:', args.dut, args.buddy)
+
+print(f"DUT port selected: {args.dut}\nBuddy port selected: {args.buddy}")
+
+if args.root != None:
+    print(f"Root path selected: {args.root}")
+else:
+    print(f"Tests file selected: {args.tests}")
 
 # After flashing the DUT, wait this long before checking for success strings
 timeout = 3
 
 captured_msgs = []
 
-# For each dictionary in data, load the appropriate elfs onto dut and buddy and check for success strings
+# For each dictionary in test_data, load the appropriate elfs onto dut and buddy and check for success strings
 def main():
     global captured_msgs
 
@@ -37,11 +48,14 @@ def main():
     # If the user passed a root directory to expand into files, create the json based on this
     if args.root != None:
         load_json(args.root)
-    
-    # Read files for testing from files.json
-    with open("new_files.json", "r") as file:
-        test_data = json.load(file)
+        with open("tests.json", "r") as file:
+            test_data = json.load(file)
 
+    # Otherwise, load test_files from specified json
+    else:
+       with open(args.tests, "r") as file:
+            test_data = json.load(file) 
+    
     for element in test_data:
         # Announce test name
         print('-----', element["test_name"], '-----')
@@ -59,8 +73,7 @@ def main():
                                     verify reset exit"', \
                                     capture_output = True, text = True, shell = True)
 
-            #print(result.stdout)
-            #print(result.stderr)
+            print('\n'.join(result.stderr.splitlines()[-5:]))
 
         # Set target to dut
         set_target('dut')
@@ -75,12 +88,10 @@ def main():
                                 verify reset exit"', \
                                 capture_output = True, text = True, shell = True)
 
-        #print(result.stdout)
-        #print(result.stderr)
+        print('\n'.join(result.stderr.splitlines()[-5:-1]))
 
         # Allow timeout seconds for test files to run before checking for success strings
         sleep(timeout)
-        print(captured_msgs)
 
         if check_for_string(captured_msgs, element):
             print("TEST SUCCESFUL")
@@ -88,8 +99,6 @@ def main():
         else:
             print("TEST FAILED")
             write_json(f"Test {element["dut_file"]} failed.", "results.json")
-
-        # Add result to output json
 
         # Empty list for next test
         captured_msgs = []
@@ -104,7 +113,7 @@ def check_for_string(msg_list, element):
     
     for item in msg_list:
         if bytes(element['success_string'], 'utf-8') in item:
-            print(f"Found success string {item} in msg_list")
+            print(f"Found success string {element['success_string']} in msg_list")
             return True
     return False 
 
@@ -113,39 +122,51 @@ def set_target(target):
     target_result = subprocess.run(["cargo", "run", "target", target], cwd = "/home/louis/testing/testctl", capture_output = True, text = True)
     print(target_result.stdout)
 
-# Function for loading json with pico examples .elf files
+# Function for loading json with all .elf files contained within some root directory
+# Creates tests.json
 def load_json(root):
     # Expand root to full path
     path = os.path.expanduser(f"{root}")
 
-    file_path_list = []
+    elf_path_list = []
     dir_path_list = [path]
 
-    for _ in range(3):
-        temp_list_2 = []
-        for dir in dir_path_list:
+    while True:
+        elf_changes = 0
+        dir_changes = 0
+        temp_list = []
+
+        # Try to get the contents from every directory in dir_list
+        for path in dir_path_list:
             try:
-                temp_list = os.listdir(dir)
-                for item in temp_list:
-                    temp_list_2.append(f"{dir}/{item}")
+                expanded_list = os.listdir(path)
+                for item in expanded_list:
+                    temp_list.append(f"{path}/{item}")
             except:
                 pass
 
-        for item in temp_list_2:
-            if item not in dir_path_list:
-                dir_path_list.append(item)
-                if item.endswith(".elf"):
-                    file_path_list.append(item)
+        # Split the paths contained in temp_list into either directories or .elfs 
+        for path in temp_list:
+            if os.path.isdir(path) and path not in dir_path_list:
+                dir_path_list.append(path)
+                dir_changes += 1
+            elif path.endswith(".elf") and path not in elf_path_list:
+                elf_path_list.append(path)
+                elf_changes += 1
 
-    data = []
-    for path in file_path_list:
-        data.append({"dut_file": path, "buddy_file": "NULL", "success_string": "Hello, world!\r\n", "test_name": "TEST"})
-    
-    json_data = json.dumps(data, indent=4)
+        # If we havent added to directories list or .elf list, must be done searching
+        if elf_changes == 0 and dir_changes == 0:
+            break
+
+    tests = []
+    for path in elf_path_list:
+        tests.append({"dut_file": path, "buddy_file": "NULL", "success_string": "", "test_name": "TEST"})
+
+    json_tests = json.dumps(tests, indent=4)
 
     # Load files into json
-    with open("new_files.json", "w") as file:
-        file.write(json_data)
+    with open("tests.json", "w") as file:
+        file.write(json_tests)
 
 # Function for adding results to json
 def write_json(new_data, filename):
